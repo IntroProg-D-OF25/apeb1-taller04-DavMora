d0eb4efa4ff51da81337d530a519e297589670e6
DavMora <147663479+DavMora@users.noreply.github.com>
2024-11-04 20:50:10 -0500

    Update Ejercicio06.java

--- Taller04_DavidMora/src/Ejercicio06.java
+++ Taller04_DavidMora/src/Ejercicio06.java
@@ -1,23 +1,15 @@
 import java.util.Scanner;
 public class Ejercicio06 {
-
-
-
     public static void main(String[] args) {
         double montoPrestamo, interesMensual, pagoMensual;
         int plazoMeses = 12; // Plazo de 1 año en meses
         Scanner teclado = new Scanner(System.in);
-
         System.out.print("Ingrese el monto del prestamo: ");
         montoPrestamo = teclado.nextDouble();
-
         System.out.print("Ingrese el interes mensual en porcentaje: ");
         interesMensual = teclado.nextDouble() / 100;
-
-    
         pagoMensual = montoPrestamo * (interesMensual * Math.pow(1 + interesMensual, plazoMeses)) /
                       (Math.pow(1 + interesMensual, plazoMeses) - 1);
-
         System.out.println("El pago mensual es de: " + pagoMensual);
     }
 }
